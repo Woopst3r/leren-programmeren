ae18a2d5b5c4a9d63cea29ecb3cd6890b5dc83e5
Jussef <jalfartousy@gmail.com>
2022-12-01 09:27:00 +0100

    elif-statement

--- Module_2_omgaan_met/meten_is_weten.py
+++ Module_2_omgaan_met/meten_is_weten.py
@@ -2,11 +2,10 @@ a = input ('voer een getal in: ')
 b = input ('voer een getal in: ')
 
 if a > b:
-    print (f"{a} is gorter dan {b}")
-
-if b > a:
-    print (f"{b} is gorter dan {a}")
-
-
-
-
+    max = a
+    min = b
+    print (f"{max} is gorter dan {min}")
+elif b > a:
+    max = b
+    min = a
+    print (f"{max} is gorter dan {min}")
