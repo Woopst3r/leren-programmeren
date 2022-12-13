5811743e269848d80fae40e2832a497a21fc7fa0
Jussef <jalfartousy@gmail.com>
2022-12-13 11:11:48 +0100

    even

---
+++ Module_3_keer_op_keer/Rocket.py
@@ -0,0 +1,5 @@
+countdown = int(input("Fanaf waneer wil je dat de raket af telt?: "))
+for tafels in range (countdown):   
+    (countdown -1)    
+    print (countdown - tafels)
+
---
+++ Module_3_keer_op_keer/Tijd.py
@@ -0,0 +1,4 @@
+for tijd in range (1,13):   
+    print(f"{tijd} am") 
+for tijds in range (1,13):  
+    print(f"{tijds} pm")
